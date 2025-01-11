d0f5bc00aeffb2fb422fe24a8c2ce555db3213a3
Franco Ariel Canavese <fcanavese@hotmail.com>
2025-01-11 12:20:03 +0100

    Exercise_4.10

--- tests/tests4.8_4.12.test.js
+++ tests/tests4.8_4.12.test.js
@@ -32,6 +32,27 @@ test('verifies that ID property of the blog posts is named id', async () => {
   assert(blogs[0]._id === undefined)
 })
 
+test('verify POST new blog', async () => {
+  const newBlog = {
+    title: 'Test Blog',
+    author: 'Test Author',
+    url: 'https://test.com',
+    likes: 10,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogAtEnd = await helper.blogsInDb()
+  assert.strictEqual(blogAtEnd.length, helper.initialBlogs.length+1)
+
+  const authors = blogAtEnd.map(n => n.author)
+  assert(authors.includes('Test Author'))
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
