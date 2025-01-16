2dc77b519a6ed480b72c32d86ee52e9f59c9bb5f
Franco Ariel Canavese <fcanavese@hotmail.com>
2025-01-16 18:27:37 +0100

    Exercise_4.17

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,10 +1,11 @@
 const blogsRouter = require('express').Router()
 const { response } = require('express')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 // ---- GET ------
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id:1 }) //promesa
   response.json(blogs)
 })
 
@@ -12,12 +13,15 @@ blogsRouter.get('/', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.find({})
+
   const blog = new Blog(
     {
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes || 0,
+      user: user[0].id
     }
   )
   
@@ -26,8 +30,15 @@ blogsRouter.post('/', async (request, response) => {
   } else if(!blog.url) {
     response.status(400).end()
   } else {
-    const saveBlog = await blog.save()
-    response.status(201).json(saveBlog)
+    const savedBlog = await blog.save()
+
+    if (!user[0].blogs) {
+      user[0].blogs = [];  // Inicializa el array si no existe
+    }
+    user[0].blogs = user[0].blogs.concat(savedBlog._id)
+    await user[0].save()
+
+    response.status(201).json(savedBlog)
   }
 
 })
--- controllers/users.js
+++ controllers/users.js
@@ -2,11 +2,13 @@ const bcryptjs = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+// ---- GET ------
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}) // .populate('blogs', { title: 1, author: 1 })
+  const users = await User.find({}).populate('blogs', { title: 1, author: 1, url:1, id: 1 })
   response.json(users)
 })
 
+// ---- POST -----
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
